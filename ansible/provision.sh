58acdfed7bdff8df6c1fd7743f5173aff4628891
Erich Rodrigues Ferrares <erich.rodriguesf@gmail.com>
2016-05-03 18:07:13 -0300

    remove ngrok for a while on master

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -5,9 +5,6 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 
-sudo ansible-galaxy install manala.apt
-sudo ansible-galaxy install manala.ngrok
-
 sudo mkdir -p /etc/ansible
 printf '[vagrant]\nlocalhost\n' | sudo tee /etc/ansible/hosts > /dev/null
 
